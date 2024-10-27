0f03a7a996ba65917e1d4cae185eb5865fd3c587
supastishn <omareeto2012@hotmail.com>
2024-10-27 17:58:08 +0300

    add dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,23 +1,24 @@
 <script>
   import { SquarePlus  } from 'lucide-svelte';
+  import Navbar from "../../components/common/navbar.svelte"
 </script>
 
 <!-- Progress bar-->
-
-<div class="mt-32 text-2xl">
-<div class="text-center w-screen flex flex-col justify-center content-center">
-<div class="w-[40%] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-  <div class="bg-blue-600 h-2.5 rounded-full w-[45%]"></div>
+<Navbar />
+<div class="mt-24 text-2xl flex flex-col">
+<div class=" bg-indigo-200 w-[50%] text-center flex flex-col justify-center self-center items-center p-3 rounded-full">
+<div class="w-[70%] bg-gray-200 rounded-full h-5 dark:bg-gray-700">
+  <div class="bg-blue-600 h-full rounded-full w-[0%]"></div>
 </div>
 
-<div class="">
-<p class="text-left">0/500 ML calls used</p>
+<div class="mt-2 rounded">
+<p class="text-center">0/500 ML calls used</p>
 </div>
 </div>
-<div class="mt-16">
+<div class="mt-24">
 <div class="flex flex-row justify-around min-w-screen">
 <h1 class="color-white text-4xl">Your models</h1>
-<button class="bg-cyan-600 inline rounded p-2"><SquarePlus class="inline"/> Create New</button>
+<button class="bg-cyan-600 inline rounded-xl p-2"><SquarePlus class="inline"/> Create New</button>
 </div>
 </div>
 </div>
